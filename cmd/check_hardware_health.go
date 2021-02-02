d5a6d8579ddbdee84d1aa4b19ff04a5d9cd2b2aa
babos77 <niklas.schon@gmx.net>
2021-02-02 13:40:44 +0100

    check hardware-health for oracle-acme

---
+++ cmd/check_hardware_health.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"github.com/inexio/thola/core/request"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	addDeviceFlags(checkHardwareHealthCMD)
+	checkCMD.AddCommand(checkHardwareHealthCMD)
+}
+
+var checkHardwareHealthCMD = &cobra.Command{
+	Use:   "hardware-health",
+	Short: "Check hardware-health of a device.",
+	Long:  "Check hardware-health of a device and return various performance data.",
+	Run: func(cmd *cobra.Command, args []string) {
+		r := request.CheckHardwareHealthRequest{
+			CheckDeviceRequest: getCheckDeviceRequest(),
+		}
+		handleRequest(&r)
+	},
+}
