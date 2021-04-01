f3cb1f45b74bc3be31a862931a3cc8a43a887d6e
Toberd <tberdin@gmx.de>
2021-04-01 16:19:30 +0200

    Changed timos communicator

--- core/communicator/timos.go
+++ core/communicator/timos.go
@@ -25,48 +25,127 @@ func (c *timosCommunicator) GetInterfaces(ctx context.Context) ([]device.Interfa
 		return nil, errors.New("no device connection available")
 	}
 
-	ports := ".1.3.6.1.4.1.6527.3.1.2.4.3.2.1.5"
-	descriptions, err := con.SNMP.SnmpClient.SNMPWalk(ctx, ports)
+	// get mapping from every ifIndex to a description
+	indexDescriptions, err := getPhysPortDescriptions(ctx)
 	if err != nil {
-		return nil, errors.Wrap(err, "error during snmp walk")
+		return nil, errors.Wrap(err, "failed to read phys port descriptions")
 	}
-	for _, response := range descriptions {
-		valueString, err := response.GetValueString()
+
+	// apply description mapping to the default interfaces
+	interfaces = normalizeTimosInterfaces(interfaces, indexDescriptions)
+
+	// get all sap interfaces
+	sapDescriptionsOID := ".1.3.6.1.4.1.6527.3.1.2.4.3.2.1.5"
+	sapDescriptions, err := con.SNMP.SnmpClient.SNMPWalk(ctx, sapDescriptionsOID)
+	if err != nil {
+		return nil, errors.Wrap(err, "snmpwalk failed")
+	}
+
+	for _, response := range sapDescriptions {
+		special, err := response.GetValueString()
 		if err != nil {
 			return nil, errors.Wrap(err, "couldn't get string value")
 		}
-		if valueString == "" {
-			continue
+
+		// construct description
+		suffix := strings.Split(strings.TrimPrefix(response.GetOID(), sapDescriptionsOID), ".")
+		physIndex := suffix[2]
+		subID := suffix[3]
+		description, ok := indexDescriptions[physIndex]
+		if !ok {
+			return nil, errors.New("invalid physical index")
 		}
-		index := strings.TrimPrefix(response.GetOID(), ports)
-		in, err := con.SNMP.SnmpClient.SNMPGet(ctx, ".1.3.6.1.4.1.6527.6.2.2.2.8.1.1.1.4"+index)
+		description += ":" + subID
+		if special != "" {
+			description += " " + special
+		}
+
+		// construct index
+		subIndex, err := strconv.ParseUint(physIndex+subID, 0, 64)
 		if err != nil {
-			return nil, errors.Wrap(err, "error during snmp get")
+			return nil, errors.Wrap(err, "couldn't get index from strings")
 		}
-		inStr, err := in[0].GetValueString()
+
+		// retrieve inbound
+		inbound, err := getCounterFromSnmpGet(ctx, ".1.3.6.1.4.1.6527.6.2.2.2.8.1.1.1.4."+suffix[1]+"."+physIndex+"."+subID)
 		if err != nil {
-			return nil, errors.Wrap(err, "couldn't get string value")
+			return nil, errors.Wrap(err, "failed to retrieve inbound counter")
 		}
-		trafficIn, err := strconv.ParseUint(inStr, 0, 64)
+
+		//retrieve outbound
+		outbound, err := getCounterFromSnmpGet(ctx, ".1.3.6.1.4.1.6527.6.2.2.2.8.1.1.1.6."+suffix[1]+"."+physIndex+"."+subID)
 		if err != nil {
-			return nil, errors.Wrap(err, "failed to parse snmp response")
+			return nil, errors.Wrap(err, "failed to retrieve outbound counter")
 		}
-		out, err := con.SNMP.SnmpClient.SNMPGet(ctx, ".1.3.6.1.4.1.6527.6.2.2.2.8.1.1.1.6"+index)
-		if err != nil {
-			return nil, errors.Wrap(err, "error during snmp get")
-		}
-		outStr, err := out[0].GetValueString()
-		if err != nil {
-			return nil, errors.Wrap(err, "couldn't get string value")
-		}
-		trafficOut, err := strconv.ParseUint(outStr, 0, 64)
-		if err != nil {
-			return nil, errors.Wrap(err, "failed to parse snmp response")
-		}
+
+		// build logical interface
 		interfaces = append(interfaces, device.Interface{
-			IfDescr: &valueString,
-			SAP:     &device.SAPInterface{Inbound: &trafficIn, Outbound: &trafficOut}})
+			IfIndex: &subIndex,
+			IfDescr: &description,
+			SAP:     &device.SAPInterface{Inbound: &inbound, Outbound: &outbound},
+		})
 	}
 
 	return interfaces, nil
+}
+
+// getPhysPortDescriptions returns a mapping from every ifIndex to a description.
+// This description is different and shorter than the ifDescription.
+func getPhysPortDescriptions(ctx context.Context) (map[string]string, error) {
+	con, ok := network.DeviceConnectionFromContext(ctx)
+	if !ok || con.SNMP == nil {
+		return nil, errors.New("no device connection available")
+	}
+
+	physPortsOID := ".1.3.6.1.4.1.6527.3.1.2.2.4.2.1.6.1"
+	physPorts, err := con.SNMP.SnmpClient.SNMPWalk(ctx, physPortsOID)
+	if err != nil {
+		return nil, errors.Wrap(err, "snmpwalk failed")
+	}
+
+	indexDescriptions := make(map[string]string)
+
+	for _, response := range physPorts {
+		description, err := response.GetValueString()
+		if err != nil {
+			return nil, errors.Wrap(err, "couldn't get string value")
+		}
+		index := strings.TrimPrefix(response.GetOID(), physPortsOID+".")
+		indexDescriptions[index] = description
+	}
+	return indexDescriptions, nil
+}
+
+func getCounterFromSnmpGet(ctx context.Context, oid string) (uint64, error) {
+	con, ok := network.DeviceConnectionFromContext(ctx)
+	if !ok || con.SNMP == nil {
+		return 0, errors.New("no device connection available")
+	}
+
+	res, err := con.SNMP.SnmpClient.SNMPGet(ctx, oid)
+	if err != nil || len(res) != 1 {
+		return 0, errors.Wrap(err, "snmpget failed")
+	}
+	resString, err := res[0].GetValueString()
+	if err != nil {
+		return 0, errors.Wrap(err, "couldn't parse snmp response")
+	}
+	resCounter, err := strconv.ParseUint(resString, 0, 64)
+	if err != nil {
+		return 0, errors.Wrap(err, "couldn't parse snmp response")
+	}
+	return resCounter, nil
+}
+
+func normalizeTimosInterfaces(interfaces []device.Interface, descriptions map[string]string) []device.Interface {
+	for _, interf := range interfaces {
+		descr, ok := descriptions[strconv.FormatUint(*interf.IfIndex, 10)]
+		if !ok {
+			continue
+		}
+		*interf.IfDescr = descr
+		*interf.IfName = descr
+	}
+
+	return interfaces
 }
