6bdeb6cbc78c1a349552a96e70301a2248f9ec1e
mikameyer <zekrom644@live.de>
2021-08-27 09:17:25 +0200

    renamed integration test

--- main.go
+++ main.go
@@ -2,7 +2,8 @@ package main
 
 import "github.com/inexio/thola/cmd"
 
-// go generate generates the mocks needed for tests. this requires mockery as a dependency
+// 'go generate' generates the mocks needed for the tests. This requires mockery as a dependency.
+// Run 'go generate ./...' in the root folder of the project to generate all mocks.
 //go:generate go get github.com/vektra/mockery/v2
 
 func main() {
