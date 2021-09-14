e4354429ce7135fd28c395fe93d5382b6f3101bf
mikameyer <zekrom644@live.de>
2021-09-14 17:01:49 +0200

    refactor ironware cpu load check

--- config/codecommunicator/ironware.go
+++ config/codecommunicator/ironware.go
@@ -5,6 +5,7 @@ import (
 	"github.com/inexio/thola/internal/device"
 	"github.com/inexio/thola/internal/network"
 	"github.com/pkg/errors"
+	"github.com/rs/zerolog/log"
 	"strconv"
 	"strings"
 )
@@ -19,16 +20,46 @@ func (c *ironwareCommunicator) GetCPUComponentCPULoad(ctx context.Context) ([]de
 	if !ok || con.SNMP == nil {
 		return nil, errors.New("no device connection available")
 	}
-	responses, err := con.SNMP.SnmpClient.SNMPWalk(ctx, ".1.3.6.1.4.1.1991.1.1.2.11.1.1.5")
+
+	// snAgentCpuUtil100thPercent
+	cpuUtilizationOID := ".1.3.6.1.4.1.1991.1.1.2.11.1.1.6"
+	precision := 100.0
+	cpuUtilization, err := con.SNMP.SnmpClient.SNMPWalk(ctx, cpuUtilizationOID)
 	if err != nil {
-		return nil, errors.Wrap(err, "snmpwalk failed")
+		log.Ctx(ctx).Debug().Err(err).Msg("walking 'snAgentCpuUtil100thPercent' failed")
+
+		// snAgentCpuUtilValue
+		cpuUtilizationOID = ".1.3.6.1.4.1.1991.1.1.2.11.1.1.4"
+		precision = 1.0
+		cpuUtilization, err = con.SNMP.SnmpClient.SNMPWalk(ctx, cpuUtilizationOID)
+		if err != nil {
+			log.Ctx(ctx).Debug().Err(err).Msg("walking 'snAgentCpuUtilValue' failed")
+			return nil, errors.Wrap(err, "getting CPU utilization failed")
+		}
 	}
+
+	indexSlotNum := make(map[string]string)
+	// snAgentCpuUtilSlotNum
+	slotNumOID := ".1.3.6.1.4.1.1991.1.1.2.11.1.1.1"
+	slotNum, err := con.SNMP.SnmpClient.SNMPWalk(ctx, slotNumOID)
+	if err != nil {
+		log.Ctx(ctx).Debug().Err(err).Msg("walking 'snAgentCpuUtilSlotNum' failed")
+	} else {
+		for _, num := range slotNum {
+			res, err := num.GetValueString()
+			if err != nil {
+				return nil, errors.Wrap(err, "couldn't get string value")
+			}
+			indexSlotNum[strings.TrimPrefix(num.GetOID(), slotNumOID)] = res
+		}
+	}
+
 	var cpus []device.CPU
-	for _, response := range responses {
-		if !strings.HasSuffix(response.GetOID(), "300") {
+	for _, cpuUtil := range cpuUtilization {
+		if !strings.HasSuffix(cpuUtil.GetOID(), "300") {
 			continue
 		}
-		valueString, err := response.GetValueString()
+		valueString, err := cpuUtil.GetValueString()
 		if err != nil {
 			return nil, errors.Wrap(err, "couldn't get string value")
 		}
@@ -36,7 +67,13 @@ func (c *ironwareCommunicator) GetCPUComponentCPULoad(ctx context.Context) ([]de
 		if err != nil {
 			return nil, errors.Wrap(err, "failed to parse snmp response")
 		}
-		cpus = append(cpus, device.CPU{Load: &value})
+		value /= precision
+		cpu := device.CPU{Load: &value}
+		if num, ok := indexSlotNum[strings.TrimPrefix(cpuUtil.GetOID(), cpuUtilizationOID)]; ok {
+			cpu.Label = &num
+		}
+		cpus = append(cpus, cpu)
 	}
+
 	return cpus, nil
 }
