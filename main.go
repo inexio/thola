5f305d06f1d54bc2884cc24c42998213d468d577
babos77 <niklas.schon@gmx.net>
2022-02-24 18:54:06 +0100

    fix mockery version to 2.10.0

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "github.com/inexio/thola/cmd"
 
 // 'go generate' generates the mocks needed for the tests. This requires mockery as a dependency.
 // Run 'go generate ./...' in the root folder of the project to generate all mocks.
-//go:generate go get github.com/vektra/mockery/v2
+//go:generate go get github.com/vektra/mockery/v2@v2.10.0
 
 func main() {
 	cmd.Execute()
